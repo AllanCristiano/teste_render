e2def3a7907ccd6a5fa4ae9283f91ceace237dbc
allan <drmundo920@gmail.com>
2024-09-14 12:32:25 -0300

    feat: complemente

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -11,5 +13,11 @@ public class Controller {
     public String getMethodName() {
         return new String("Hello World");
     }
+
+    @GetMapping("/books")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     
 }
